78f01a5651f88cedcf587eebab3764db41e38f3e
armand-vs <armandvs@telesure.co.za>
2022-05-10 11:18:15 +0200

    add signout to ProfileIcon

--- src/components/profile/ProfileIcon.js
+++ src/components/profile/ProfileIcon.js
@@ -30,7 +30,7 @@ class ProfileIcon extends React.Component {
                     </DropdownToggle>
                     <DropdownMenu  className='b--transparent shadow-5' style={{marginTop: '20px', backgroundColor: 'rgba(255, 255, 255, 0.5'}}>
                         <DropdownItem>View Profile</DropdownItem>
-                        <DropdownItem>Signout</DropdownItem>
+                        <DropdownItem onClick={() => this.props.onRouteChange('signout')}>Sign Out</DropdownItem>
                     </DropdownMenu>
                 </Dropdown>
         </div>
