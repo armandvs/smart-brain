28b80c880c4c96880d72bd005346fd7cc2b1116f
armand-vs <armandvs@telesure.co.za>
2022-05-25 15:31:06 +0200

    signin response update

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -27,8 +27,8 @@ class Signin extends React.Component {
       })
     })
       .then(response => response.json())
-      .then(user => {
-        if (user.id) {
+      .then(data => {
+        if (data.userId) {
           this.props.loadUser(user)
           this.props.onRouteChange('home');
         }
